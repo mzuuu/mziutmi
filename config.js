ba20e030983532f5a57dd3b8c2e24888b535fc43
mzuuu <109956228+mzuuu@users.noreply.github.com>
2022-11-09 18:24:54 +0700

    message

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: ' Hee loo ! ',
+    introDesc: `Chồng có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha 🥺`,
+    btnIntro: 'OK',
+    title: 'Chồng biết Em iu Chồng nhìu lắm phải không ? 😙',
+    desc: 'Không trả lời hoặc thoát ra làm tró :> ',
+    btnYes: 'Có, Em iuu Chồng 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho chồng biết đi vì sao Em iu Chồng 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Mzu ngon giaiii 😋😋😋',
+    mess: 'Chồng biết mà, Chồng cũng iu em lắm 🥰. Iu Iem 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Mzu nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/tmiiu'
+}
